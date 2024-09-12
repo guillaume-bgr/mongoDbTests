32593b92b1d3c24ac3d64946e2044c5adc465a2e
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-09-12 08:45:05 +0200

    adding auth access on weather routes

---
+++ middleware/authToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+function authToken(req, res, next) {
+    let authToken = req.headers && req.headers.authorization;
+    if (authToken) {
+        authToken = authToken.split(' ');
+        try {
+            var decoded = jwt.verify(authToken[1], process.env.SECRET_KEY);
+            res.tokenId = decoded.id;
+        } catch(err) {
+            if(err.name == 'TokenExpiredError'){
+                return res.status(500).json({ message: 'Token has expired', err: 'TokenExpiredError' })
+            }
+        }
+    } else {
+    return res.status(500).json({ message: 'No token given', err: 'NoTokenError'})
+    }
+    next()
+}
+
+module.exports = authToken;
--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,8 +1,9 @@
 const WeatherController = require('../controllers/WeatherController.js');
+const authToken = require('../middleware/authToken.js');
 module.exports = (app) => {
     app.group("/weathers", (router) => {
-        router.get("/data", WeatherController.getData);
-        router.get("/data/:id", WeatherController.getDataByStationId);
-        router.post("/data", WeatherController.setData);
+        router.get("/data", authToken, WeatherController.getData);
+        router.get("/data/:id", authToken, WeatherController.getDataByStationId);
+        router.post("/data", authToken, WeatherController.setData);
     });
 }
